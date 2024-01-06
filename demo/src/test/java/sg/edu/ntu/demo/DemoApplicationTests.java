ea02b190a57d686fee3302722d1aacdf0686413d
jqzy91 <jamesquek91@live.com.sg>
2024-01-06 12:18:32 +0800

    Initial Spring Apllication

---
+++ demo/src/test/java/sg/edu/ntu/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.ntu.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
